32816d360ebada5e7e1d296fa870a656f437d618
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-02 02:20:16 +0900

    feat: ロジックの完成

--- main001.go
+++ main001.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// 文法書的にはinterface{} を使用すると思うのですが、最新のGoのバージョンではany型が使用できるらしいので使用しました
+	n := []any{1, "2", 10, "11"}
+	// 文字列を格納するためのスライス
+	stringN := []string{}
+
+	for _, v := range n {
+		switch v := v.(type) {
+		// int型の場合は文字列に変換
+		case int:
+			stringN = append(stringN, strconv.Itoa(v))
+		case string:
+			stringN = append(stringN, v)
+		}
+	}
+
+	for _, z := range stringN {
+		// 文字数カウント
+		count := utf8.RuneCountInString(z)
+		if count == 2 {
+			fmt.Println(z)
+		} else {
+			fmt.Println("0" + z)
+		}	
+	}
 }
+
+
