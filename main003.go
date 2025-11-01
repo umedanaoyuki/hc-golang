03de82ab04c2ac373c055a74a71288b5116a12b8
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-02 05:30:15 +0900

    feat: ロジックの完成

--- main003.go
+++ main003.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
+
+
+type MyIntSlice []int
+
+func (m MyIntSlice) Unique() MyIntSlice {
+	// 重複を削除したスライス
+	n := []int{}
+	
+	for _, v := range m {
+		// スライスの中にvの値がすでに含まれているかどうかのチェック
+		result := slices.Contains(n, v)
+		if !result {
+			n = append(n, v)
+		}
+	}
+	return n
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	m := MyIntSlice{1, 2, 2, 3, 3, 3, 4, 5}
+	fmt.Println(m.Unique())
 }
