3ec4e06c2fc758e7b420f29b9663d1004388d0ee
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-02 05:48:20 +0900

    問題文の写し

--- main004.go
+++ main004.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	users := []User{
+		{"tarou", 33},
+		{"zirou", 22},
+		{"itirou", 11},
+	}
+
+	for _, user := range users {
+		user.Age = 44
+	}
+
+	fmt.Printf("%v", users) // どうなる？
 }
