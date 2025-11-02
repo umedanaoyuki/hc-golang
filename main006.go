46706f1a8e3b649151f2069fc80d25ce267ebe24
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-03 02:04:59 +0900

    feat: ロジックの追加（string関数）

--- main006.go
+++ main006.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type MyInt int
+
+// MyInt型に関連したStringのメソッドを追加して、そこで""hoge"を返却するようにする
+func (m MyInt) String() string {
+	return "hoge"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	var m MyInt = 3
+
+	fmt.Println(m)
 }
