794fdd7deb6bf72149f7fa0e56cee00f9733ebff
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-05 17:22:04 +0900

    feat: ファイル読み取りの処理の追加

--- go_insert_db/main.go
+++ go_insert_db/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//実行時に引数を受け取る
+	// ファイル名の指定
+	args := os.Args
+	// ファイルは一つだけと限定する
+	if len(args) != 2 {
+		fmt.Println("引数の数が間違っています")
+		os.Exit(1)
+	}
+
+	fmt.Println("ファイル読み取り処理を開始します")
+	// ファイルをOpenする
+	f, err := os.Open(args[1])
+	// 読み取り時の例外処理
+	if err != nil {
+		fmt.Println("error")
+	}
+	// 関数が終了した際に確実に閉じるようにする
+	defer f.Close()
 }
