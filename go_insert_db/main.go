f616a78d69fb1d90ca8f284e901851df4fb94136
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-05 22:16:06 +0900

    refactor: 不要なコメントの削除

--- go_insert_db/main.go
+++ go_insert_db/main.go
@@ -7,18 +7,6 @@ import (
 	"log"
 	"os"
 )
-
-//  {
-// 	"user": {
-// 	  "age": 22,
-// 	  "name": "tarou",
-// 	  "role": "student"
-// 	},
-// 	"dist": "PostgreSQL",
-// 	"level": "info",
-// 	"msg": "test",
-// 	"src": "backend",
-// 	"time": "2021-08-01T00:05:05Z"
   
 type User struct {
 	Age int `json:"age"`
@@ -34,14 +22,6 @@ type LogData struct {
 	Src string `json:"src"`
 	Time string `json:"time"`
  }
-
-//  type Person struct {
-// 	Name   string
-// 	Age    int
-// 	gender string
-// }
-
-// var logDatas []LogData
 
 func main() {
 	//実行時に引数を受け取る
@@ -62,19 +42,10 @@ func main() {
 		fmt.Println("error")
 	}
 
-	// p := Person{
-	// 	Name: "Mike",
-	// 	Age:  20,
-	// 	gender: "male",
-	// }
-
 	file , err := os.Open(logFile.Name())
-	// scanner := bufio.NewScanner(file)
 	if err != nil {
 		log.Fatal("ファイルを開けませんでした", err)
 	}
-
-	// fmt.Println(file)
 
 	defer file.Close()
 
@@ -93,38 +64,7 @@ func main() {
 		}
 		// %+v を使用するとフィールド名も表示される
 		fmt.Printf("%+v\n", logDatas)
-		// type LogData struct {
-		// 	user User
-		// 	dist string
-		// 	level string
-		// 	msg string
-		// 	src string
-		// 	time string
-		//  }
-
-		// entry := LogData{
-		// 	user: User{
-		// 		age: line["user"].age,
-		// 		name: line["user"].name,
-		// 		role: line["user"].role,
-		// 	},
-		// 	dist: line["dist"],
-		// 	level: line["level"].(string),
-		// 	msg: line["msg"].(string),
-		// 	src: line["src"].(string),
-		// 	time: line["time"].(string),
-		// }
 	}
-	// }
-	// err = scanner.Err()   
-	// if err != nil {
-	// 	fmt.Println("error")
-	// 	// プロセス終了（失敗）
-	// 	os.Exit(1)
-	// }
-
-	// m, _ := json.Marshal(p)
-	// fmt.Println(string(m))
 
 	// 関数が終了した際に確実に閉じるようにする
 	defer file.Close()
